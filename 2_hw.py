3d48b40de14e8b64a2569c94eb841bd2e2a5f994
danzel238 <djban778@gmail.com>
2025-07-29 21:50:04 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,25 @@
 
+def task_1():
+    a = 1 #int
+    print('a = ', type(a))
+    b = 1.456 #float
+    print('b = ', type(b))
+    c = 'abcdefg' #STR
+    print('c = ', type(c))
+    d = [1,2,3,4] #list
+    print('d =', type(d))
+    e = True
+    print('e = ', type(e))
+
+task_1()
+
+
+def task_2():
+    a = [1, 2, 3, 5, 8, 13, 21]
+    print (a[0:3])
+task_2()
+#последовательность Фибоначчи
+
+def task_3(num:int):
+    return num **2
+print (task_3(4))
